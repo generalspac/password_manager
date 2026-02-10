a4618c42e032f549e21c03a0e9fde18d710dea5b
generalspac <mamadoury20046@gmail.com>
2026-02-10 21:58:42 +0100

    Remove unused 'algo' function and docstring
    Remove the placeholder function 'algo' and its docstring.

--- algorithme.py
+++ algorithme.py
@@ -1,9 +1,1 @@
-def algo():
-    """cree une interface ou il est possible d'entrer son le site, le mail,le mot de passe,
-        demandez a l'utilisateur si tout est correct,si oui , on enregistre dans un fichier ou
-        on cree un dataframe avec pandas, qu'on convertira en csv ou extension que l'on veut(un peu complexe),
-        puis on va creer une partie ou l'utilisateur doit saisir un mot de passe pour voir tout ce qui est enregistre
-        dans ces le fichier de ses donnees personnels.
 
-
-    """
